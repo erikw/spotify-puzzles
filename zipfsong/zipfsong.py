053ea8f845059dd676b8f1d38adbaa7be7ac0b42
Erik Westrup <erik.westrup@gmail.com>
2013-08-16 00:50:51 +0200

    Added appropriate nil checks.

--- zipfsong/zipfsong.py
+++ zipfsong/zipfsong.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # zipfsong: Get the best songs assuming the Zipfian distribution.
 
 import sys
@@ -24,21 +24,25 @@ def total_nbr_plays(songs):
     return sum([song.plays for song in songs])
 
 def calc_quality(songs, total_plays):
-    z_1 = total_plays / sum([1/i for i in range(1, len(songs) + 1)])
+    z_1 = total_plays / sum([float(1)/i for i in range(1, len(songs) + 1)])
     for song in songs:
-        song.quality = song.plays * song.song_no / z_1 
+        if z_1 > 0:
+            song.quality = song.plays * song.song_no / z_1
+        else:
+            song.quality = 0
 
 def get_best(songs, num_select):
-    songs_sorted = sorted(songs, reverse=True, key= lambda o: o.quality)
-    return songs_sorted[0:num_select]
+    songs_sorted = sorted(songs, reverse = True, key= lambda o: (o.quality, -o.song_no))
+    return songs_sorted[:num_select]
 
 def main():
     songs, num_select = read_input()
-    total_plays = total_nbr_plays(songs)
-    calc_quality(songs, total_plays)
-    best_songs = get_best(songs, num_select)
-    for quality_song in best_songs:
-        print(quality_song)
+    if len(songs) > 0:
+        total_plays = total_nbr_plays(songs)
+        calc_quality(songs, total_plays)
+        best_songs = get_best(songs, num_select)
+        for quality_song in best_songs:
+            print(quality_song)
 
 if __name__ == "__main__":
     main()
